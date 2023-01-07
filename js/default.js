a07d6bfff88670f35d6f39d10795c94242673ca8
ArniLollipop <bocunahero@gmail.com>
2023-01-07 16:30:50 +0600

    add js in modal

--- js/default.js
+++ js/default.js
@@ -1,6 +1,37 @@
-let isModal = false;
+let countS = 0;
+
+    let isModal = false;
     let isLogIn = false;
     let burgerMenu = false;
+    let isRecovery = false;
+    let isPassword = false;
+
+
+    function recovery(){
+      if(!isRecovery){
+        document.getElementById("logIn").classList.add("hidden");
+        document.getElementById("password").classList.remove("hidden")
+        isRecovery = !isRecovery;
+      }
+      else{
+        document.getElementById("logIn").classList.remove("hidden");
+        document.getElementById("password").classList.add("hidden")
+        isRecovery = !isRecovery;
+      }
+    }
+
+    function passwordOpen(){
+      if (!isPassword){
+        document.getElementById("password").classList.add("hidden");
+        document.getElementById("recovery").classList.remove("hidden")
+        isPassword = !isPassword;
+      }
+      else{
+        document.getElementById("password").classList.remove("hidden");
+        document.getElementById("recovery").classList.add("hidden")
+        isPassword = !isPassword;
+      }
+    }
 
     function Burger(){
       if (!burgerMenu){
@@ -8,6 +39,7 @@ let isModal = false;
         document.getElementById("burger2").classList.add("opacity-0", "rotate-360")
         document.getElementById("burger3").classList.add("-rotate-45", "-translate-x-[9px]", "translate-y-1")
         document.getElementById("menu").classList.remove("translate-x-full")
+        document.getElementById("main").classList.add("hidden")
         burgerMenu = !burgerMenu
       }
       else{
@@ -15,6 +47,7 @@ let isModal = false;
         document.getElementById("burger2").classList.remove("opacity-0", "rotate-360")
         document.getElementById("burger3").classList.remove("-rotate-45", "-translate-x-[9px]", "translate-y-1")
         document.getElementById("menu").classList.add("translate-x-full")
+        document.getElementById("main").classList.remove("hidden")
         burgerMenu = !burgerMenu
       }
     }
@@ -41,11 +74,14 @@ let isModal = false;
       else{
         isLogIn = true
         signUp();
+        isPassword = true
+        passwordOpen();
+        isRecovery= true
+        recovery();
         document.getElementById("modal").classList.add("hidden")
         isModal = !isModal
       }
     }
-    //
 
     let count = 0;
 
